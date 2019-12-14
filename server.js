706af4a74577ef7a01017e9225965bcef0fee6bb
kokoagb <kokoagb@gmail.com>
2019-12-14 11:58:49 +0100

    add server.js with search, typeahead and best_podcasts

---
+++ server.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+const dotenv = require('dotenv')
+const express = require('express')
+const app = express()
+
+dotenv.config()
+
+const endpoints = ['/search', '/typeahead', '/best_podcasts']
+
+const http = axios.create({
+  baseURL: 'https://listen-api.listennotes.com/api/v2',
+  headers: {
+    'X-ListenAPI-Key': process.env.LISTEN_API_KEY,
+  },
+})
+
+endpoints.forEach(endpoint => {
+  app.get(`/api${endpoint}`, async (req, res) => {
+    const response = await makeRequest(endpoint, req.query, req.params)
+    res.send(response.data)
+  })
+})
+
+function makeRequest(endpoint, query, { id }) {
+  if (id) endpoint.replace(':id', id)
+  return http.get(endpoint, {
+    params: query,
+  })
+}
+
+app.listen(3001, () => {
+  console.log('Listening on port 3001')
+})
