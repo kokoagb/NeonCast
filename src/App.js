1ee27e4311c7c4994ced1bc716be339a1657a86e
kokoagb <kokoagb@gmail.com>
2019-12-16 18:47:36 +0100

    import podcastList component in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PodcastList from 'modules/podcastList/PodcastListPage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PodcastList></PodcastList>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
