b4f58e24bd9e11e8f32e9dc0c554c25973f7ed70
kokoagb <kokoagb@gmail.com>
2019-12-25 12:01:23 +0100

    add image loading gradient animation

--- src/components/PodcastImage.js
+++ src/components/PodcastImage.js
@@ -14,7 +14,7 @@ const StyledImg = styled.img`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   background: linear-gradient(0deg, #ffffff, #dfdfdf);
   background-size: 400% 400%;
-  animation: ${bgAnimation} 1s ease infinite;
+  animation: ${bgAnimation} 3s ease infinite;
 `
 
 function PodcastImage({ src, alt }) {
@@ -23,8 +23,8 @@ function PodcastImage({ src, alt }) {
 
   useEffect(() => {
     const img = document.createElement('img')
-    // img.addEventListener('load', imgEventListener)
-    // img.addEventListener('error', imgEventListener)
+    img.addEventListener('load', imgEventListener)
+    img.addEventListener('error', imgEventListener)
     img.src = src
 
     return () => {
