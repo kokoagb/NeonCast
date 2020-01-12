cd8c6f0b4d32ac47a22675661796b7a847279380
kokoagb <kokoagb@gmail.com>
2020-01-12 11:08:43 +0100

    add play/pause functionality to player

--- src/modules/header/Header.js
+++ src/modules/header/Header.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
 import Logo from './Logo'
+import SearchBox from './SearchBox'
 
 const StyledHeader = styled.header`
   grid-area: header;
+  display: grid;
+  grid-template-columns: 250px auto;
+
+  .search-box-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 `
 
 function header() {
   return (
     <StyledHeader>
       <Logo />
+      <div className="search-box-wrapper">
+        <SearchBox />
+      </div>
     </StyledHeader>
   )
 }
--- src/modules/player/Player.js
+++ src/modules/player/Player.js
@@ -1,21 +1,40 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
-import { Loader } from 'react-feather'
+import { Loader, Play, Pause } from 'react-feather'
+import { throttle } from 'lodash'
+import { getFormattedTime } from 'common/utils'
 
 const StyledDiv = styled.div`
   grid-area: player;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100px;
-  background-color: white;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  z-index: 1;
+
+  img {
+    float: left;
+    width: 100px;
+    height: 100px;
+  }
+
+  button {
+    border: none;
+    padding: 0;
+    margin: 0;
+    background: none;
+    cursor: pointer;
+  }
+
+  .monospace {
+    font-family: monospace;
+  }
 `
 
 function Player() {
   const nowPlaying = useSelector(state => state.nowPlaying)
   const [isLoading, setIsLoading] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [currentTime, setCurrentTime] = useState(0)
   const playerEl = useRef(null)
 
   useEffect(() => {
@@ -27,15 +46,53 @@ function Player() {
   const handleCanPlay = () => {
     setIsLoading(false)
     playerEl.current.play()
+    setIsPlaying(true)
   }
+
+  const handlePlayPauseClick = () => {
+    if (playerEl.current.paused) {
+      playerEl.current.play()
+      setIsPlaying(true)
+    } else {
+      playerEl.current.pause()
+      setIsPlaying(false)
+    }
+  }
+
+  const handleTimeUpdate = throttle(() => {
+    setCurrentTime(getFormattedTime(playerEl.current.currentTime))
+  }, 1000)
 
   if (!nowPlaying) return null
 
+  const loader = isLoading ? <Loader className="icon spin" /> : null
+  const playPause = !isLoading ? (
+    <button onClick={handlePlayPauseClick}>
+      {isPlaying ? <Pause className="icon" /> : <Play className="icon" />}
+    </button>
+  ) : null
+
+  const totalTime = getFormattedTime(nowPlaying.audio_length_sec)
+
   return (
     <StyledDiv>
-      {isLoading && <Loader className="icon spin" />}
-      <p>{nowPlaying.title}</p>
-      <audio ref={playerEl} onCanPlay={handleCanPlay}></audio>
+      <img src={nowPlaying.image} alt={nowPlaying.title} />
+
+      <div>
+        {loader}
+        {playPause}
+      </div>
+      <div>{nowPlaying.title}</div>
+      <div>
+        <small className="monospace">
+          {currentTime}/{totalTime}
+        </small>
+      </div>
+      <audio
+        ref={playerEl}
+        onCanPlay={handleCanPlay}
+        onTimeUpdate={handleTimeUpdate}
+      ></audio>
     </StyledDiv>
   )
 }
