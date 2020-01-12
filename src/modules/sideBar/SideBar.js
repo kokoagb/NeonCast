9a26e10cec4083f495dc06310e9b0deee2fd4066
kokoagb <kokoagb@gmail.com>
2020-01-12 11:06:17 +0100

    highlight active item in sidebar

--- src/modules/sideBar/SideBar.js
+++ src/modules/sideBar/SideBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useLocation, Link } from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom'
 
 const StyledDiv = styled.div`
   grid-area: sidebar;
@@ -9,20 +9,34 @@ const StyledDiv = styled.div`
 const StyledUl = styled.ul`
   list-style-type: none;
   padding: 0;
+
+  li {
+    padding: 0.5rem;
+
+    &.active {
+    background-color: black;
+    border-radius: 0 2rem 2rem 0;
+
+    & a {
+      color: white;
+    }
+  }
 `
 
 function Sidebar() {
-  const location = useLocation()
-
   const menuItems = [
     {
       name: 'Trending Podcasts',
       location: '/',
+      match: useRouteMatch('/'),
     },
   ]
 
   const renderedMenuItems = menuItems.map(item => (
-    <li key={item.location}>
+    <li
+      key={item.location}
+      className={item.match && item.match.isExact ? 'active' : ''}
+    >
       <Link to={item.location}>{item.name}</Link>
     </li>
   ))
