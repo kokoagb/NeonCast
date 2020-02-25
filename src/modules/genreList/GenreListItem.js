7e324c32a037f133fd276809f9a4552f2fa22c8d
kokoagb <kokoagb@gmail.com>
2020-02-25 20:23:55 +0100

    add genre background images

--- src/modules/genreList/GenreListItem.js
+++ src/modules/genreList/GenreListItem.js
@@ -4,17 +4,27 @@ import { Link } from 'react-router-dom'
 
 function GenreListItem({ className, genre }) {
   return (
-    <div className={className}>
+    <div
+      className={className}
+      style={{
+        backgroundImage: `url(https://picsum.photos/seed/${genre.id}/300/150.jpg?blur=5)`,
+      }}
+    >
       <Link to={`/genres/${genre.id}`}>{genre.name}</Link>
     </div>
   )
 }
 
 export default styled(GenreListItem)`
-  height: 5rem;
   border-radius: 0.5rem;
   background-color: #b9b8ff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+
+  a {
+    display: block;
+    height: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-shadow: 0 0 15px white;
+  }
 `
