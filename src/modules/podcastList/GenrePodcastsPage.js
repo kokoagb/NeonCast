06416e546d7f30f0b9cc9e0d10c8299d9e4cb918
kokoagb <kokoagb@gmail.com>
2020-02-25 20:46:32 +0100

    load genres on genre podcasts page + display genre name

--- src/modules/podcastList/GenrePodcastsPage.js
+++ src/modules/podcastList/GenrePodcastsPage.js
@@ -3,18 +3,28 @@ import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchBestPodcasts } from 'modules/podcastList/podcastListSlice'
 import PodcastListPage from './PodcastListPage'
+import { fetchGenres } from 'modules/genreList/genresSlice'
 
 function TrendingPage() {
   const { genre_id } = useParams()
   const dispatch = useDispatch()
   const { podcasts, pagination } = useSelector(state => state.podcastList)
+  const { genres } = useSelector(state => state.genres)
   const { page_number } = pagination
+
+  const genre = genres.find(genre => genre.id === Number(genre_id))
+
+  useEffect(() => {
+    dispatch(fetchGenres())
+  }, [dispatch])
 
   useEffect(() => {
     dispatch(fetchBestPodcasts(page_number, genre_id))
   }, [genre_id, page_number, dispatch])
 
-  return <PodcastListPage title={genre_id} podcasts={podcasts} />
+  return (
+    <PodcastListPage title={genre ? genre.name : null} podcasts={podcasts} />
+  )
 }
 
 export default TrendingPage
